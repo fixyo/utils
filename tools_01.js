5bc043d24e62589310b23a230ea86ee107041e84
ec <chenchao@bz.cn>
2020-04-10 17:07:05 +0800

    debounc throttle

--- tools_01.js
+++ tools_01.js
@@ -14,8 +14,56 @@ export const debounce = function(fn, delay = 100) {
   }   
 }
 
-export const throttle = function() {
 
+/**
+ * @name 节流
+ * @param {*} fn 
+ * @param {*} delay 
+ * @description delay时间之内 多次触发 只执行第一次 
+ */
+export const throttle = function(fn, delay) {
+  let flag = true 
+  return function () {
+    if (!flag) return 
+    flag = false 
+    setTimeout(() => { 
+      fn.apply(this, arguments)
+      flag = true
+    }, delay)
+  }
 }
 
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @description 得到两个数组的交集, 两个数组的元素为数值或字符串
+ */
+export const getIntersection = (arr1, arr2) => {
+  let len = Math.min(arr1.length, arr2.length)
+  let i = -1
+  let res = []
+  while(++i < len) {
+    if (arr1.indexOf(arr2[i]) > -1) res.push(arr2[i])
+  }
+  return res 
+}
+
+
+/**
+ * 
+ * @param {*} arr1 
+ * @param {*} arr2 
+ * @description 两个数组的并集
+ */
+export const getUnion = (arr1, arr2) => {
+  // 数组去重
+  // return Array.from(new Set(...arr1, arr2))
+  ret = []
+  let newArr = [...arr1, ...arr2]
+  newArr.forEach(item => {
+    if (ret.indexOf(item) === -1 ) ret.push(item)
+    
+  })
+  return ret 
+}
 
